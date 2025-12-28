83e89fe1ee7ba25bf2fb6f3bd6db0c0e92f8fef2
maia <maiaycn2028@student.cis.edu.hk>
2025-12-28 23:04:33 +0800

    Maia Submission

--- src/code/ImageManipulation.java
+++ src/code/ImageManipulation.java
@@ -1,95 +1,188 @@
 package code;
 
+import image.APImage;
 import image.Pixel;
+
+import javax.swing.*;
 
 public class ImageManipulation {
 
-    /** CHALLENGE 0: Display Image
-     *  Write a statement that will display the image in a window
-     */
+    private static int getAverageColour(Pixel pixel) {
+        int red = pixel.getRed();
+        int green = pixel.getGreen();
+        int blue = pixel.getBlue();
+
+        int average = (red + green + blue) / 3;
+        return average;
+    }
+
+    public static void grayScale(String pathOfFile) {
+        APImage image = new APImage(pathOfFile);
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width; column++) {
+                Pixel pixel = image.getPixel(column, row);
+
+                int average = getAverageColour(pixel);
+
+                pixel.setRed(average);
+                pixel.setGreen(average);
+                pixel.setBlue(average);
+
+
+            }
+        }
+        image.draw();
+    }
+
+    public static void blackAndWhite(String pathOfFile) {
+        APImage image = new APImage(pathOfFile);
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width; column++) {
+                Pixel pixel = image.getPixel(column, row);
+
+                int average = getAverageColour(pixel);
+
+                if (average < 128) {
+                    pixel.setRed(0);
+                    pixel.setGreen(0);
+                    pixel.setBlue(0);
+
+                }
+                if (average >= 128) {
+                    pixel.setRed(255);
+                    pixel.setGreen(255);
+                    pixel.setBlue(255);
+
+                }
+
+            }
+
+        }
+        image.draw();
+    }
+
+    public static void edgeDetection(String pathToFile, int threshold) {
+        APImage image = new APImage(pathToFile);
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        APImage end = new APImage(width, height);
+
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width; column++) {
+                Pixel pixel = image.getPixel(column, row);
+                int average = getAverageColour(pixel);
+
+                boolean isEdge = false;
+
+                if (column > 0) {
+                    Pixel left = image.getPixel(column - 1, row);
+                    int averageOfLeft = getAverageColour(left);
+                    if (Math.abs(average - averageOfLeft) > threshold) {
+                        isEdge = true;
+                    }
+                }
+
+                if (row < height - 1) {
+                    Pixel below = image.getPixel(column, row + 1);
+                    int averageOfBelow = getAverageColour(below);
+                    if (Math.abs(average - averageOfBelow) > threshold) {
+                        isEdge = true;
+                    }
+                }
+
+                Pixel endPixel = end.getPixel(column, row);
+                if (isEdge) {
+                    endPixel.setRed(0);
+                    endPixel.setGreen(0);
+                    endPixel.setBlue(0);
+                } else {
+                    endPixel.setRed(255);
+                    endPixel.setGreen(255);
+                    endPixel.setBlue(255);
+                }
+            }
+        }
+
+        end.draw();
+
+    }
+
+    public static void reflectImage(String pathToFile) {
+        APImage image = new APImage(pathToFile);
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width / 2; column++) {
+
+                Pixel pixel = image.getPixel(column, row);
+                int mirror = width - 1 - column;
+                Pixel right = image.getPixel(mirror, row);
+
+                int red = pixel.getRed();
+                int green = pixel.getGreen();
+                int blue = pixel.getBlue();
+                int rightRed = right.getRed();
+                int rightGreen = right.getGreen();
+                int rightBlue = right.getBlue();
+                pixel.setRed(rightRed);
+                pixel.setGreen(rightGreen);
+                pixel.setBlue(rightBlue);
+                right.setRed(red);
+                right.setGreen(green);
+                right.setBlue(blue);
+
+
+            }
+        }
+        image.draw();
+    }
+
+    public static void rotateImage(String pathToFile) {
+        APImage image = new APImage(pathToFile);
+        int width = image.getWidth();
+        int height = image.getHeight();
+        APImage rotated = new APImage(height, width);
+
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width; column++) {
+
+                Pixel pixel = image.getPixel(column, row);
+                int newCol = height - 1 - row;
+                int newRow = column;
+                Pixel end = rotated.getPixel(newCol, newRow);
+
+                end.setRed(pixel.getRed());
+                end.setGreen(pixel.getGreen());
+                end.setBlue(pixel.getBlue());
+
+            }
+        }
+        rotated.draw();
+    }
+
+
+
     public static void main(String[] args) {
 
+        APImage image = new APImage("cyberpunk2077.jpg");
+        System.out.println(image);
+        image.draw();
+
+        grayScale("cyberpunk2077.jpg");
+        blackAndWhite("cyberpunk2077.jpg");
+        edgeDetection("cyberpunk2077.jpg",20);
+        reflectImage("cyberpunk2077.jpg");
+        rotateImage("cyberpunk2077.jpg");
 
     }
 
-    /** CHALLENGE ONE: Grayscale
-     *
-     * INPUT: the complete path file name of the image
-     * OUTPUT: a grayscale copy of the image
-     *
-     * To convert a colour image to grayscale, we need to visit every pixel in the image ...
-     * Calculate the average of the red, green, and blue components of the pixel.
-     * Set the red, green, and blue components to this average value. */
-    public static void grayScale(String pathOfFile) {
-
-    }
-
-    /** A helper method that can be used to assist you in each challenge.
-     * This method simply calculates the average of the RGB values of a single pixel.
-     * @param pixel
-     * @return the average RGB value
-     */
-    private static int getAverageColour(Pixel pixel) {
-        return 0;
-    }
-
-    /** CHALLENGE TWO: Black and White
-     *
-     * INPUT: the complete path file name of the image
-     * OUTPUT: a black and white copy of the image
-     *
-     * To convert a colour image to black and white, we need to visit every pixel in the image ...
-     * Calculate the average of the red, green, and blue components of the pixel.
-     * If the average is less than 128, set the pixel to black
-     * If the average is equal to or greater than 128, set the pixel to white */
-    public static void blackAndWhite(String pathOfFile) {
-
-    }
-
-    /** CHALLENGE Three: Edge Detection
-     *
-     * INPUT: the complete path file name of the image
-     * OUTPUT: an outline of the image. The amount of information will correspond to the threshold.
-     *
-     * Edge detection is an image processing technique for finding the boundaries of objects within images.
-     * It works by detecting discontinuities in brightness. Edge detection is used for image segmentation
-     * and data extraction in areas such as image processing, computer vision, and machine vision.
-     *
-     * There are many different edge detection algorithms. We will use a basic edge detection technique
-     * For each pixel, we will calculate ...
-     * 1. The average colour value of the current pixel
-     * 2. The average colour value of the pixel to the left of the current pixel
-     * 3. The average colour value of the pixel below the current pixel
-     * If the difference between 1. and 2. OR if the difference between 1. and 3. is greater than some threshold value,
-     * we will set the current pixel to black. This is because an absolute difference that is greater than our threshold
-     * value should indicate an edge and thus, we colour the pixel black.
-     * Otherwise, we will set the current pixel to white
-     * NOTE: We want to be able to apply edge detection using various thresholds
-     * For example, we could apply edge detection to an image using a threshold of 20 OR we could apply
-     * edge detection to an image using a threshold of 35
-     *  */
-    public static void edgeDetection(String pathToFile, int threshold) {
-
-    }
-
-    /** CHALLENGE Four: Reflect Image
-     *
-     * INPUT: the complete path file name of the image
-     * OUTPUT: the image reflected about the y-axis
-     *
-     */
-    public static void reflectImage(String pathToFile) {
-
-    }
-
-    /** CHALLENGE Five: Rotate Image
-     *
-     * INPUT: the complete path file name of the image
-     * OUTPUT: the image rotated 90 degrees CLOCKWISE
-     *
-     *  */
-    public static void rotateImage(String pathToFile) {
-
-    }
 
 }
